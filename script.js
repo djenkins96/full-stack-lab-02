f6f84cca3eaafac0c97777d0e56bbc24cd5f9813
djenkins96 <derrick.jenkins@me.com>
2017-05-31 18:52:05 -0400

    updated color scheme

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ for(i = 0; i < friends.length; i++){
         if (j > 1){
             console.log(j +" lines of code in the file, " + j +" lines of code; " + friends[i] + " strikes one out, clears it all out," + j +" lines of code in the file");
     } else {
-            console.log("1 line of code in the file, 1 line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file")
+            console.log("1 line of code in the file, 1 line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file");
         }
     }
 }
