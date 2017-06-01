a35afacdf01e029912b9248d3db0d688b369cacf
djenkins96 <derrick.jenkins@me.com>
2017-06-01 09:38:03 -0400

    updated to countdown correctly

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var friends = ["Bob", "Linda", "Gene", "Tina", "Louise"]
 
-for(i = 0; i < friends.length; i++){
-    console.log(friends[i]);
-    for(j = 99; j > 0; j--) {
-        if (j > 1){
-            console.log(j +" lines of code in the file, " + j +" lines of code; " + friends[i] + " strikes one out, clears it all out," + j +" lines of code in the file");
+for(var i = 0; i < friends.length; i++){
+    console.log(friends[i].toUpperCase() + ':');
+    for(var j = 99; j > 0; j--) {
+        if (j > 2){
+            console.log(j +" lines of code in the file, " + j +" lines of code; " + friends[i] + " strikes one out, clears it all out," + (j-1) +" lines of code in the file");
+    } else if(j === 2){
+            console.log(j +" lines of code in the file, " + j +" lines of code; " + friends[i] + " strikes one out, clears it all out," + (j-1) +" line of code in the file");
     } else {
             console.log("1 line of code in the file, 1 line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file");
         }
